ab84bb9deddebe08bc93340ed2a921f380fa1dde
Alan Yaniger <alan@tkos.co.il>
2021-08-12 20:54:15 +0300

    Add tests for yang patch

--- test/test_restconf_yang_patch.sh
+++ test/test_restconf_yang_patch.sh
@@ -7,14 +7,17 @@
 # Magic line must be first in script (see README.md)
 s="$_" ; . ./lib.sh || if [ "$s" = $0 ]; then exit 0; else return 0; fi
 
-echo "...skipped: YANG_PATCH NYI"
-if [ "$s" = $0 ]; then exit 0; else return 0; fi
+#echo "...skipped: YANG_PATCH NYI"
+#if [ "$s" = $0 ]; then exit 0; else return 0; fi
     
 APPNAME=example
     
 cfg=$dir/conf.xml
 startupdb=$dir/startup_db
 fjukebox=$dir/example-jukebox.yang
+fyangpatch=$dir/ietf-yang-patch.yang
+finterfaces=$dir/ietf-interfaces.yang
+fexample=$dir/clixon-example.yang
     
 # Define default restconfig config: RESTCONFIG
 RESTCONFIG=$(restconf_config user false)
@@ -102,6 +105,15 @@ cat <<EOF > $dir/example-system.yang
    }
 EOF
 
+# Yang Patch spec (fyangpatch must be set)
+. ./yang-patch.sh
+
+# Interfaces spec (finterfaces must be set)
+. ./interfaces.sh
+
+# clixon example spec (fexample must be set)
+. ./example.sh
+
 # Common Jukebox spec (fjukebox must be set)
 . ./jukebox.sh
 
@@ -132,53 +144,174 @@ fi
 new "wait restconf"
 wait_restconf
 
-# RFC 8072 A.1.1 
-REQ='<yang-patch xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-patch">
-        <patch-id>add-songs-patch</patch-id>
-        <edit>
-          <edit-id>edit1</edit-id>
-          <operation>create</operation>
-          <target>/song=Bridge%20Burning</target>
-          <value>
-            <song xmlns="http://example.com/ns/example-jukebox">
-              <name>Bridge Burning</name>
-              <location>/media/bridge_burning.mp3</location>
-              <format>MP3</format>
-              <length>288</length>
-            </song>
-          </value>
-        </edit>
-        <edit>
-          <edit-id>edit2</edit-id>
-          <operation>create</operation>
-          <target>/song=Rope</target>
-          <value>
-            <song xmlns="http://example.com/ns/example-jukebox">
-              <name>Rope</name>
-              <location>/media/rope.mp3</location>
-              <format>MP3</format>
-              <length>259</length>
-            </song>
-          </value>
-        </edit>
-        <edit>
-          <edit-id>edit3</edit-id>
-          <operation>create</operation>
-          <target>/song=Dear%20Rosemary</target>
-          <value>
-            <song xmlns="http://example.com/ns/example-jukebox">
-              <name>Dear Rosemary</name>
-              <location>/media/dear_rosemary.mp3</location>
-              <format>MP3</format>
-              <length>269</length>
-            </song>
-          </value>
-        </edit>
-      </yang-patch>'
+# Modify several interfaces with a YANG patch, testing create, merge, and delete
+REQ='{
+  "ietf-yang-patch:yang-patch": {
+    "patch-id": "alan-test-patch",
+    "edit": [
+      {
+        "edit-id": "edit-1",
+        "operation": "create",
+        "target": "/interface=eth1",
+        "value": {
+          "interface": [
+            { 
+              "name": "eth1",
+              "type": "clixon-example:eth",
+              "enabled": "false"
+            }
+          ]
+        }
+      },
+      {
+        "edit-id": "edit-2",
+        "operation": "create",
+        "target": "/interface=eth2",
+        "value": {
+          "interface": [
+            {
+              "name": "eth2",
+              "type": "clixon-example:eth",
+              "enabled": "false"
+            }
+          ]
+        }
+      },
+      {
+        "edit-id": "edit-3",
+        "operation": "create",
+        "target": "/interface=eth4",
+        "value": {
+          "interface": [
+            {
+              "name": "eth4",
+              "type": "clixon-example:eth",
+              "enabled": "false"
+            }
+          ]
+        }
+      },
+      {
+        "edit-id": "edit-4",
+        "operation": "merge",
+        "target": "/interface=eth2",
+        "value": {
+          "interface": [
+            { 
+              "enabled": "true"
+            }
+          ]
+        }
+      },
+      {
+        "edit-id": "edit-5",
+        "operation": "delete",
+        "target": "/interface=eth1"
+      }
+    ]
+  }
+}'
+new "RFC 8072 YANG Patch: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X PATCH -H 'Content-Type: application/yang-patch+json' -H 'Accept: application/yang-patch+json' $RCPROTO://localhost/restconf/data/ietf-interfaces:interfaces -d "$REQ")" 0 "HTTP/$HVER 204 No Content"
+#
+# Create artist in jukebox example
+REQ='{"example-jukebox:artist":[{"name":"Foo Fighters"}]}'
+new "RFC 8072 YANG Patch jukebox example 1: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X POST -H 'Content-Type: application/yang-data+json' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/library -d "$REQ")" 0 "HTTP/$HVER 201 Created"
 
-new "RFC 8072 A.1.1 Add resources: Error."
-expectpart "$(curl -u andy:bar $CURLOPTS -X PATCH -H 'Content-Type: application/yang-patch+xml' -H 'Accept: application/yang-data+xml' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/library/artist=Foo%20Fighters/album=Wasting%20Light -d "$REQ")" 0 "HTTP/$HVER 409"
+# Create album in jukebox example
+REQ='<album xmlns="http://example.com/ns/example-jukebox"><name>Wasting Light</name><year>2011</year></album>'
+new "RFC 8072 YANG Patch jukebox example 2: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X POST -H 'Content-Type: application/yang-data+xml' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/library/artist=Foo%20Fighters -d "$REQ")" 0 "HTTP/$HVER 201 Created"
 
+# Add fields to album in jukebox example
+REQ='{"example-jukebox:album":[{"name":"Wasting Light","genre":"example-jukebox:alternative","year":2011}]}'
+new "RFC 8072 YANG Patch jukebox example 3: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X PUT -H 'Content-Type: application/yang-data+json' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/library/artist=Foo%20Fighters/album=Wasting%20Light -d "$REQ")" 0 "HTTP/$HVER 204 No Content"
+
+# Uncomment to get info about album in jukebox example
+#new "RFC 8072 YANG Patch jukebox example get 2: Error."
+#expectpart "$(curl -u andy:bar $CURLOPTS -X GET $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/library)" 0 "HTTP/$HVER 201 OK"
+
+# Add songs to playlist in jukebox example
+REQ="{\"example-jukebox:song\":[{\"index\":1,\"id\":\"/example-jukebox:jukebox/library/artist[name='Foo Fighters']/album[name='Wasting Light']/song[name='Rope']\"}]}"
+new "RFC 8072 YANG Patch jukebox example 4: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X POST -H 'Content-Type: application/yang-data+json' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/playlist=Foo-One?insert=first -d "$REQ")" 0 "HTTP/$HVER 201 Created"
+
+# Add song at end of playlist
+REQ="{\"example-jukebox:song\":[{\"index\":2,\"id\":\"/example-jukebox:jukebox/library/artist[name='Foo Fighters']/album[name='Wasting Light']/song[name='Bridge Burning']\"}]}"
+new "RFC 8072 YANG Patch jukebox example 5: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X POST -H 'Content-Type: application/yang-data+json' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/playlist=Foo-One?insert=last -d "$REQ")" 0 "HTTP/$HVER 201 Created"
+
+# Add song at end of playlist
+REQ="{\"example-jukebox:song\":[{\"index\":4,\"id\":\"/example-jukebox:jukebox/library/artist[name='Foo Fighters']/album[name='Wasting Light']/song[name='Still More Rope']\"}]}"
+new "RFC 8072 YANG Patch jukebox example 6: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X POST -H 'Content-Type: application/yang-data+json' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/playlist=Foo-One?insert=last -d "$REQ")" 0 "HTTP/$HVER 201 Created"
+
+# Add song at end of playlist
+REQ="{\"example-jukebox:song\":[{\"index\":3,\"id\":\"/example-jukebox:jukebox/library/artist[name='Foo Fighters']/album[name='Wasting Light']/song[name='More Rope']\"}]}"
+new "RFC 8072 YANG Patch jukebox example 7: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X POST -H 'Content-Type: application/yang-data+json' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/playlist=Foo-One?insert=last -d "$REQ")" 0 "HTTP/$HVER 201 Created"
+
+# Run YANG patch on the playlist, testing "insert after" and "insert before"
+REQ='{
+  "ietf-yang-patch:yang-patch": {
+    "patch-id": "alan-test-patch-jukebox",
+    "edit": [
+      {
+        "edit-id": "edit-2",
+        "operation": "insert",
+        "target": "/song=5",
+        "point": "/song=1",
+        "where" : "after",
+        "value": {
+          "example-jukebox:song": [
+            {
+              "index": 5,
+              "id" : "Rope Galore"
+            }
+          ]
+        }
+      },
+      {
+        "edit-id": "edit-3",
+        "operation": "insert",
+        "target": "/song=6",
+        "point": "/song=4",
+        "where" : "before",
+        "value": {
+          "example-jukebox:song": [
+            {
+              "index": 6,
+              "id" : "How Much Rope Does a Man Need"
+            }
+          ]
+        }
+      },
+      {
+        "edit-id": "edit-2",
+        "operation": "insert",
+        "target": "/song=24",
+        "point": "/song=6",
+        "where" : "after",
+        "value": {
+          "example-jukebox:song": [
+            {
+              "index": 24,
+              "id" : "The twenty fourth song"
+            }
+          ]
+        }
+      }
+    ]
+  }
+}'
+new "RFC 8072 YANG Patch jukebox example: Error."
+expectpart "$(curl -u andy:bar $CURLOPTS -X PATCH -H 'Content-Type: application/yang-patch+json' -H 'Accept: application/yang-patch+json' $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/playlist=Foo-One -d "$REQ")" 0 "HTTP/$HVER 201 Created"
+
+# Uncomment to get info about playlist in jukebox example
+#new "RFC 8072 YANG Patch jukebox example get : Error."
+#expectpart "$(curl -u andy:bar $CURLOPTS -X GET $RCPROTO://localhost/restconf/data/example-jukebox:jukebox/playlist=Foo-One)" 0 "HTTP/$HVER 201 OK"
 
 if [ $RC -ne 0 ]; then
     new "Kill restconf daemon"
